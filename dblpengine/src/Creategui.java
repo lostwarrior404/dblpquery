fd06ad113ff19f3ae33c75a4e764c4855d2902b6
Saksham Suri <sakshamsuri9001@gmail.com>
2016-11-08 17:56:50 +0530

    Update Creategui.java

--- dblpengine/src/Creategui.java
+++ dblpengine/src/Creategui.java
@@ -5,5 +5,24 @@ public class GUI {
     public JLabel titletop;
     public JPanel leftpane;
     public JPanel rightpane;
+    
+    
+    public GUI(){
+        myframe=new JFrame();
+    }
+    public void createGui(){//gets all the panels and panes and sets them accordingly
+        titletop=createTitletop();
+        leftpane=createLeftpane();
+        rightpane=createRightpane();
+    }
+    public JPanel createLeftpane(){
+
+    }
+    public JPanel createRightpane(){
+
+    }
+    public JLabel createTitletop(){
+
+    }
 
 }
