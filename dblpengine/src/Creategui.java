5664b6d59573e7b4ce6aa7af8f4711f9d80296bf
Saksham Suri <sakshamsuri9001@gmail.com>
2016-11-09 23:01:22 +0530

    Progress in creating the gui

--- dblpengine/src/Creategui.java
+++ dblpengine/src/Creategui.java
@@ -9,20 +9,25 @@ public class GUI {
     
     public GUI(){
         myframe=new JFrame();
+        createGui();
     }
     public void createGui(){//gets all the panels and panes and sets them accordingly
         titletop=createTitletop();
         leftpane=createLeftpane();
         rightpane=createRightpane();
+        myframe.setLayout(new FlowLayout());
     }
     public JPanel createLeftpane(){
-
+         
     }
     public JPanel createRightpane(){
 
     }
     public JLabel createTitletop(){
-
+        JLabel temp=new JLabel;
+        temp.setText("DBLP Query Engine");
+        temp.setFont(new Font("Arial", Font.PLAIN, 14));
+        return temp;
     }
 
 }
