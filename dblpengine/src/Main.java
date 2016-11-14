62d3a4ad9e299d271b01301c202f1f3f2534cfd1
Saksham Suri <sakshamsuri9001@gmail.com>
2016-11-14 15:10:31 +0530

    Run the parser using this

---
+++ dblpengine/src/Main.java
@@ -0,0 +1,12 @@
+package jo;
+
+/**
+ * Created by Saksham on 11/13/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        Parser p=new Parser();
+        System.setProperty("jdk.xml.entityExpansionLimit", "0");
+        p.parse("dblp.xml");
+    }
+}
