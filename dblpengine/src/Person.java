8c1d2197ec2b16f01e488eeff6b4bebd86d68a24
saksham15082 <sakshamsuri9001@gmail.com>
2016-11-26 16:01:29 +0530

    update

--- dblpengine/src/Person.java
+++ dblpengine/src/Person.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 /**
  * Created by Saksham on 11/26/2016.
  */
 public class Person {
-    ArrayList<Publication> queries = 
+    ArrayList<Publication> queries = new ArrayList<Publication>;
+    ArrayList<String> wwwnames=new ArrayList<String>();
 }
--- dblpengine/src/Publication.java
+++ dblpengine/src/Publication.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.StringJoiner;
+
 /**
  * Created by Saksham on 11/26/2016.
  */
 public class Publication {
 
-    private String author;
+    private ArrayList<String> author=new ArrayList<String>();
     private String year;
     private String title;
     private String pages;
@@ -11,5 +14,38 @@ public class Publication {
     private String volume;
     private String journal;
     private String booktitle;
-    public Publication()
+    public void setAuthor(String a) {
+        author.add(a);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+    public void setYear(String a){
+        year=a;
+    }
+    public Publication(){
+
+    }
 }
