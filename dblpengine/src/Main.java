621a3a9aeb3f674f712892724308d0d892dcda25
saksham15082 <sakshamsuri9001@gmail.com>
2016-11-26 17:07:05 +0530

    update

--- dblpengine/src/Main.java
+++ dblpengine/src/Main.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by Saksham on 11/13/2016.
@@ -6,7 +7,13 @@
 public class Main {
     public static void main(String[] args){
         Parser p=new Parser();
+        Scanner in =new Scanner(System.in);
+        String name=in.nextLine();
         System.setProperty("jdk.xml.entityExpansionLimit", "0");
-        p.parse("dblp.xml");
+        ArrayList<Publication> res = p.parse("dblp.xml",name);
+        for(Publication pu: res){
+            System.out.println(p);
+        }
+//        Query1 hey=new Query1();
     }
 }
