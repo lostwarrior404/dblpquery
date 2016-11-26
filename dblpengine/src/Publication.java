030b5192b06cd9d45121bb5960f525122f2174b3
saksham15082 <sakshamsuri9001@gmail.com>
2016-11-26 15:38:03 +0530

    update

---
+++ dblpengine/src/Publication.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Saksham on 11/26/2016.
+ */
+public class Publication {
+
+    private String author;
+    private String year;
+    private String title;
+    private String pages;
+    private String url;
+    private String volume;
+    private String journal;
+    private String booktitle;
+    public Publication()
+}
