37e593031eaba8fd311d1896b71ba3910aceca45
Tushar Kataria <tushar15184@iiitd.ac.in>
2016-11-26 15:40:12 +0530

    Added Parser

--- dblpengine/src/Person.java
+++ dblpengine/src/Person.java
@@ -2,5 +2,5 @@
  * Created by Saksham on 11/26/2016.
  */
 public class Person {
-    ArrayList<Publication> queries = 
+    //ArrayList<Publication> queries =
 }
--- dblpengine/src/Query1.java
+++ dblpengine/src/Query1.java
@@ -2,4 +2,7 @@ public class Query1 {
     public void createGui(){
         System.out.println("q1");
     }
+    public void searchbyauthor(String author){
+
+    }
 }
