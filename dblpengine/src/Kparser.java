589b617c246a6d4ea4ca705c406bce62bd6196cd
Tushar Kataria <tushar15184@iiitd.ac.in>
2016-11-29 00:06:47 +0530

    Added Parser

---
+++ dblpengine/src/Kparser.java
@@ -0,0 +1,86 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Tushar Kataria on 28-Nov-16.
+ */
+public class Kparser {
+    HashMap<String,Integer> parse(String file_path){
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            SAXParser saxParser = factory.newSAXParser();
+            final ArrayList<String> result = new ArrayList<String>();
+            final HashMap<String,Integer> maps = new HashMap<String,Integer>();
+            DefaultHandler handler = new DefaultHandler(){
+                boolean publication = false;
+                boolean author_present = false;
+                String data_acc="";
+                ArrayList<String> temp = new ArrayList<String>();
+                boolean www_present = false;
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    if(     qName.equalsIgnoreCase("article")|
+                            qName.equalsIgnoreCase("inproceedings")|
+                            qName.equalsIgnoreCase("proceedings")|
+                            qName.equalsIgnoreCase("book")|
+                            qName.equalsIgnoreCase("incollection")|
+                            qName.equalsIgnoreCase("phdthesis")|
+                            qName.equalsIgnoreCase("mastersthesis") |(qName.equalsIgnoreCase("www")&& !attributes.getValue("key").matches("homepages/(.*)")) ){
+                        publication = true;
+
+                        //                    System.out.println(qName+" started:");
+                        //                    if(attributes.getLength()>0 && (k=attributes.getValue("key"))!=null){
+                        //                        System.out.println("key = "+k);
+                        //                    }
+                    }
+                    if(qName.equalsIgnoreCase("author")){
+                        author_present = true;
+                    }
+                }
+
+                public void characters(char[] ch, int start, int length) throws SAXException {
+                    if(author_present){
+                        data_acc+=new String(ch,start,length);
+                    }
+                }
+
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+                    if(publication){
+                        if(     qName.equalsIgnoreCase("article")|
+                                qName.equalsIgnoreCase("inproceedings")|
+                                qName.equalsIgnoreCase("proceedings")|
+                                qName.equalsIgnoreCase("book")|
+                                qName.equalsIgnoreCase("incollection")|
+                                qName.equalsIgnoreCase("phdthesis")|
+                                qName.equalsIgnoreCase("mastersthesis")
+                                |qName.equalsIgnoreCase("www")){
+                            publication = false;
+                        }
+
+                        if (qName.equalsIgnoreCase("author")|qName.equalsIgnoreCase("editor")) {
+                            author_present = false;
+                            if(maps.containsKey(data_acc)){
+                                maps.put(data_acc,maps.get(data_acc)+1);
+                            }
+                            else{
+                                maps.put(data_acc,1);
+                            }
+                        }
+                    }
+                    data_acc = "";
+                    //Make object here;
+                }
+            };
+            saxParser.parse(file_path, handler);
+            return maps;
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- dblpengine/src/Query2.java
+++ dblpengine/src/Query2.java
@@ -1,83 +1,46 @@
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Query2 {
-    HashMap<String,Integer> parse(String file_path,final int k){
-          SAXParserFactory factory = SAXParserFactory.newInstance();
+    ArrayList<String> parse(int k){
+        HashMap<String,Integer> hash= new Kparser().parse("dblp.xml");
+        ArrayList<String> result = new ArrayList<String>();
+        String location = "wwww.csv";//to add location
+        BufferedReader br = null;
+        String line;
+        String cvs_split = ",";
         try {
-            SAXParser saxParser = factory.newSAXParser();
-            final ArrayList<String> result = new ArrayList<String>();
-            final HashMap<String,Integer> maps = new HashMap<String,Integer>();
-            DefaultHandler handler = new DefaultHandler(){
-                boolean publication = false;
-                boolean author_present = false;
-                String data_acc="";
-                ArrayList<String> temp = new ArrayList<String>();
-                boolean www_present = false;
-                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-                    if(     qName.equalsIgnoreCase("article")|
-                            qName.equalsIgnoreCase("inproceedings")|
-                            qName.equalsIgnoreCase("proceedings")|
-                            qName.equalsIgnoreCase("book")|
-                            qName.equalsIgnoreCase("incollection")|
-                            qName.equalsIgnoreCase("phdthesis")|
-                            qName.equalsIgnoreCase("mastersthesis") |(qName.equalsIgnoreCase("www")&& !attributes.getValue("key").matches("homepages/(.*)")) ){
-                        publication = true;
-
-                        //                    System.out.println(qName+" started:");
-                        //                    if(attributes.getLength()>0 && (k=attributes.getValue("key"))!=null){
-                        //                        System.out.println("key = "+k);
-                        //                    }
-                    }
-                    if(qName.equalsIgnoreCase("author")){
-                        author_present = true;
-                    }
-                }
-
-                public void characters(char[] ch, int start, int length) throws SAXException {
-                    if(author_present){
-                        data_acc+=new String(ch,start,length);
-                    }
-                }
-
-                public void endElement(String uri, String localName, String qName) throws SAXException {
-                    if(publication){
-                    if(     qName.equalsIgnoreCase("article")|
-                            qName.equalsIgnoreCase("inproceedings")|
-                            qName.equalsIgnoreCase("proceedings")|
-                            qName.equalsIgnoreCase("book")|
-                            qName.equalsIgnoreCase("incollection")|
-                            qName.equalsIgnoreCase("phdthesis")|
-                            qName.equalsIgnoreCase("mastersthesis")
-                            |qName.equalsIgnoreCase("www")){
-                        publication = false;
-                    }
-
-                        if (qName.equalsIgnoreCase("author")|qName.equalsIgnoreCase("editor")) {
-                            author_present = false;
-                            if(maps.containsKey(data_acc)){
-                                maps.put(data_acc,maps.get(data_acc)+1);
-                            }
-                            else{
-                                maps.put(data_acc,1);
-                            }
-                        }
-                    }
-                    data_acc = "";
-                    //Make object here;
-                }
-            };
-            saxParser.parse(file_path, handler);
-            return maps;
-        }  catch (Exception e) {
+            br = new BufferedReader(new FileReader(location));
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return null;
+        try {
+            while ((line = br.readLine()) != null) {
+                ArrayList<String> argm = new ArrayList<String>(Arrays.asList(line.split(cvs_split)));
+                int sum=0;
+                for(String s:argm){
+                    if(hash.containsKey(s)){
+                        sum+=hash.remove(s);
+                    }
+                }
+                if(sum>=k){
+                    result.add(argm.get(0));
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
